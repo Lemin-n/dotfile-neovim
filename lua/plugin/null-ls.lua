b710e03957c43cfdb27fcb507f31b826a07c0e6f
lemin-n <emiliorccp@gmail.com>
2023-03-27 16:41:53 -0300

    Implement with method to null-ls.prettier

--- lua/plugin/null-ls.lua
+++ lua/plugin/null-ls.lua
@@ -4,31 +4,42 @@ M[1] = "jose-elias-alvarez/null-ls.nvim"
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local on_attach = function(client, bufnr)
-  if client.supports_method "textDocument/formatting" then
-    vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      group = augroup,
-      buffer = bufnr,
-      callback = function()
-        -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-        vim.lsp.buf.format({async = false})
-        end,
-    })
-  end
+	if client.supports_method("textDocument/formatting") then
+		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			group = augroup,
+			buffer = bufnr,
+			callback = function()
+				-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+				vim.lsp.buf.format({ async = false })
+			end,
+		})
+	end
 end
 
 M.config = function()
 	local null_ls = require("null-ls")
 	null_ls.setup({
-    	sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-	},
-	on_attach = on_attach
+		sources = {
+			null_ls.builtins.formatting.stylua,
+			null_ls.builtins.formatting.prettier.with({
+				filetypes = {
+					"javascript",
+					"typescript",
+					"css",
+					"scss",
+					"html",
+					"json",
+					"yaml",
+					"markdown",
+					"graphql",
+					"md",
+					"txt",
+				},
+			}),
+		},
+		on_attach = on_attach,
 	})
-
 end
-
-
 
 return M
